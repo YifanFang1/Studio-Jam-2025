20f450d6738b8379d120bcf22969c6206c7ab595
YifanFang1 <logan.yifan.fang@gmail.com>
2025-11-14 20:19:35 -0800

    .

---
+++ Studio Jam 2025/Assets/Arm Movement/Forearm.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Forearm : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Vector3 mousePosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
